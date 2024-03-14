56ed79792d06e2013f918b6c9b56f762a961b558
abylays <abylay.phd@gmail.com>
2024-03-14 15:54:14 +0500

    fetch improvement proposals github repo

--- governenv/settings.py
+++ governenv/settings.py
@@ -5,3 +5,4 @@ PROJECT_ROOT = Path(__file__).parent.parent
 KAIKO_API_KEY = os.environ.get("KAIKO_API_KEY")
 TALLY_API_KEY = os.environ.get("TALLY_API_KEY")
 SNAPSHOT_API_KEY = os.environ.get("SNAPSHOT_API_KEY")
+GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
---
+++ scripts/fetch_pullRequests.py
@@ -0,0 +1,78 @@
+import gzip
+import json
+import requests
+
+from governenv.constants import GITHUB_PATH_PULL_REQUESTS
+from governenv.settings import GITHUB_TOKEN
+
+# GitHub GraphQL endpoint
+GITHUB_GRAPHQL_ENDPOINT = "https://api.github.com/graphql"
+
+# GraphQL query 
+def get_github_query(after_cursor=None):
+    after = f', after: "{after_cursor}"' if after_cursor else ""
+    return f"""
+    {{
+      # fetch BIPs 
+      repository(owner: "bitcoin", name: "bips") {{
+        pullRequests(first: 100{after}) {{
+          pageInfo {{
+            endCursor
+            hasNextPage
+          }}
+          edges {{
+            node {{
+              number
+              title
+              state
+              url
+              createdAt
+              mergedAt
+              author {{
+                login    # author of the proposal
+              }}
+              labels(first: 10) {{
+                edges {{ node {{ name }} }} 
+              }}
+              comments(first: 20) {{
+                edges {{ node {{ author {{ login }} }} }} # commenters/discussants
+              }}
+              reviews(first: 10) {{
+                edges {{ node {{ author {{ login }} state }} }} # reviewers
+              }}
+            }}
+          }}
+        }}
+      }}
+    }}
+    """
+
+# Function to fetch data using GitHub's GraphQL API with pagination
+def fetch_github_data():
+    has_next_page = True
+    end_cursor = None
+    pull_requests = []
+
+    while has_next_page:
+        query = get_github_query(end_cursor)
+        headers = {"Authorization": f"bearer {GITHUB_TOKEN}"}
+        response = requests.post(GITHUB_GRAPHQL_ENDPOINT, json={'query': query}, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            prs = data['data']['repository']['pullRequests']['edges']
+            pull_requests.extend(prs)
+            pageInfo = data['data']['repository']['pullRequests']['pageInfo']
+            has_next_page = pageInfo['hasNextPage']
+            end_cursor = pageInfo['endCursor']
+        else:
+            raise Exception(f"Query failed to run by returning code of {response.status_code}")
+            break
+
+    return pull_requests
+
+# Fetch the data 
+data = fetch_github_data()
+
+# Save the data to a gzip file
+with gzip.open(GITHUB_PATH_PULL_REQUESTS, "wt") as f:
+    f.write("\n".join([json.dumps(pr['node']) for pr in data]) + "\n")
