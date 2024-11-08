7095dbbecafc2aa92c4e45e1bebdf6fd42a3a6c2
Yichen Luo <103887158+lyc0603@users.noreply.github.com>
2024-11-08 11:56:35 +0000

    update asyncio

--- governenv/prompts.py
+++ governenv/prompts.py
@@ -1,5 +1,5 @@
 """
-Prompts
+Prompts and instructions
 """
 
 IDF_PROMPT = """Given the following website HTTP response, determine \
@@ -31,3 +31,15 @@ HTTP Response:
 {http_response}
 (End of HTTP Response)
 """
+
+
+IDF_INSTRUCT = """You are a decentralized autonomous organizations (DAO) expert \
+who is proficient in evaluating forum discussions. Your output should always be \
+either Yes or No."""
+
+EVAL_INSTRUCT = """You are a decentralized autonomous organizations (DAO) expert \
+who is proficient in evaluating forum discussions. Your output format should be as follows:
+Support: {Yes/No}
+Professionalism: {Yes/No}
+Objectiveness: {Yes/No}
+Explanation: {Your explanation here}"""
--- scripts/process_sentiment.py
+++ scripts/process_sentiment.py
@@ -1,32 +1,13 @@
 import gzip
 import json
-import pickle
 import math
+import pickle
 
 import requests
 
-from governenv.constants import DATA_DIR, EXKW, HEADERS
-from governenv.instructions import IDF_INSTRUCT, EVAL_INSTRUCT
+from governenv.constants import DATA_DIR, HEADERS
 from governenv.llm import ChatGPT
-from governenv.prompts import IDF_PROMPT, EVAL_PROMPT
-
-
-def kw_filt(data: dict[str, str]) -> dict[str, str]:
-    """
-    Function to filter discussions based on keywords
-    """
-
-    return {k: v for k, v in data.items() if not any([i in v for i in EXKW])}
-
-
-def slash_filt(data: dict[str, str]) -> dict[str, str]:
-    """
-    Function to filter discussions based on slashes
-    """
-
-    # typically, a discussion has at least 4 levels of slashes
-    # if the slash count is less than 4, remove the discussion
-    return {k: v for k, v in data.items() if v.count("/") >= 4}
+from governenv.prompts import EVAL_INSTRUCT, EVAL_PROMPT, IDF_INSTRUCT, IDF_PROMPT
 
 
 if __name__ == "__main__":
